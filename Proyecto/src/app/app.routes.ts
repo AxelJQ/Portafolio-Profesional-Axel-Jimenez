6ec13761e8996a12134a0e3d638b413cbfa6c168
Axel Jimenez <ajimenezq1@ucenfotec.ac.cr>
2024-12-02 22:41:49 -0600

    Module test

--- Proyecto/src/app/app.routes.ts
+++ Proyecto/src/app/app.routes.ts
@@ -2,13 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {
-        path: 'home',
-        component: HomeComponent
-    },
-    {
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full'
-    }
+  {
+    path: '',
+    component: HomeComponent,  // This will render HomeComponent at the root path
+    pathMatch: 'full'  // Ensures this is the default route when the path is exactly '/'
+  },
+  {
+    path: 'home',
+    component: HomeComponent  // Optionally, another route for '/home'
+  }
 ];
